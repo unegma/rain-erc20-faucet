530b553e2462a267fe3713dd0e4eaf923ba78ad1
Timothy Coleman <timhc22@gmail.com>
2022-07-22 15:40:29 +0100

    Move params

--- src/components/various/TransactionsChartDeploy.tsx
+++ src/components/various/TransactionsChartDeploy.tsx
@@ -1,58 +1,58 @@
 import {Bar} from "react-chartjs-2";
 import React from "react";
 
-const options = {
-  responsive: true,
-  scales: {
-    x: {
-      title: {
-        display: true,
-        text: 'Transactions',
-        color: 'black'
-      }
-    },
-    y: {
-      title: {
-        display: true,
-        text: 'Cost (Matic)',
-        color: 'black'
-      }
-    }
-  },
-  plugins: {
-    legend: {
-      display: false,
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Upcoming Transaction Cost Ratios (Estimated MATIC Ratios based on costs at: 2022-05-30T15:32:44Z)',
-    },
-  },
-};
-
-const data = {
-  labels: ['Tx1: Deploy Token'],
-  datasets: [
-    {
-      label: '',
-      data: [1],
-      backgroundColor: 'rgba(0, 0, 0, 0)',
-    },
-    {
-      label: '',
-      data: [0.01268265], // todo base it on dynamic matic costs
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: '',
-      data: [1],
-      backgroundColor: 'rgba(0, 0, 0, 0)',
-    }
-  ],
-};
-
 export function TransactionsChartDeploy() {
+  const options = {
+    responsive: true,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Transactions',
+          color: 'black'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Cost (Matic)',
+          color: 'black'
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Upcoming Transaction Cost Ratios (Estimated MATIC Ratios based on costs at: 2022-05-30T15:32:44Z)',
+      },
+    },
+  };
+
+  const data = {
+    labels: ['Tx1: Deploy Token'],
+    datasets: [
+      {
+        label: '',
+        data: [1],
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+      },
+      {
+        label: '',
+        data: [0.01268265], // todo base it on dynamic matic costs
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '',
+        data: [1],
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+      }
+    ],
+  };
+
   return (
     <Bar options={options} data={data} />
   )
